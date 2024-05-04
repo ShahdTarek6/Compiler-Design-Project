c58ed308201b2126d34fea0dc2d3149ef6aa18ef
ShahdTarek6 <138148116+ShahdTarek6@users.noreply.github.com>
2024-05-04 23:23:08 +0300

    work without variable

--- phase1.js
+++ phase1.js
@@ -1,6 +1,7 @@
 function scan() {
     var inputCode = document.getElementById("input").value;
-    var tokenList = inputCode.match(/('[^'\n]*')|#.+\n|\S+|\s+/g);
+    // Updated regular expression to capture whole words, symbols, and individual characters
+    var tokenList = inputCode.match(/('[^'\n]*')|#.+\n|\b(int|float|string|double|bool|char|for|while|if|do|return|break|continue|end)\b|\b\d+(\.\d+)?\b|\S|;/g);
 
     var output = document.getElementById("output");
     output.innerHTML = "";
@@ -13,16 +14,18 @@ function scan() {
     tokenList.forEach(function(token) {
         if (/^(\d+(\.\d+)?)$/.test(token)) {
             output.innerHTML += `(number, '${token}')<br>`;
-        } else if (/^(<=|<|>|=|>=|!=|==|!|\*|%|\+|-)$/.test(token)) {
-            output.innerHTML += `(operator, '${token}')<br>`;
-        } else if (/^(def|if|return|elif|else|for|in)$/.test(token)) {
-            output.innerHTML += `(reserved, '${token}')<br>`;
-        } else if (/^[\[\]():,{}]+$/.test(token)) {
-            output.innerHTML += `(symbol, '${token}')<br>`;
-        } else if (/^'[^'\n]*'$|^"[^"\n]*"$/.test(token)) {
-            output.innerHTML += `(string, ${token})<br>`;
-        } else {
+        } else if (/^(int|float|string|double|bool|char)$/.test(token)) {
             output.innerHTML += `(identifier, '${token}')<br>`;
+        } else if (/^(for|while|if|do|return|break|continue|end)$/.test(token)) {
+            output.innerHTML += `(reserved, '${token}')<br>`;
+        } else if (/^[a-zA-Z]+$/.test(token)) {
+            output.innerHTML += `(variable, '${token}')<br>`;
+        } else if (/^[\+\-\/\%\*]$/.test(token)) {
+            output.innerHTML += `(operator, '${token}')<br>`;
+        } else if (/^[\(\{\[\)\}\]\,\;]$/.test(token)) {
+            output.innerHTML += `(symbol, '${token}')<br>`;
+        } else {
+            output.innerHTML += `(unknown, '${token}')<br>`;
         }
     });
 }
